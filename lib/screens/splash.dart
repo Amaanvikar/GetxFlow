ac80ef280bd50de2b2a4dfb3bc127c648a8006d0
Aman <amaanvikar8313@gmail.com>
2025-03-10 12:10:12 +0530

    Add: get dependency and updated in splashscreen using getx

---
+++ lib/screens/splash.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1), () {
+      Get.offNamed('/home');
+    });
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 10,
+          shadowColor: Colors.black.withOpacity(0.2),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.grey.withOpacity(0.5), width: 1),
+            ),
+            child: Center(
+              child: Text(
+                'WELCOME',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
